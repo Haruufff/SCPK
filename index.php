af1815de01ab82293fd6bbc1cb27591968300f6a
Haruufff <52081019+Haruufff@users.noreply.github.com>
2025-08-21 13:08:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (empty($_SESSION['login']))
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SPK - GAMELAB.ID</title>
+  <title>TITLE</title>
   <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="assets/css/styles.min.css" />
   <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
